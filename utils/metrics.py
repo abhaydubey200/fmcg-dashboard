6944dd69c35720c741024e212220993ba765c0ed
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-31 00:22:12 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,17 +1,10 @@
-# utils/metrics.py
-import pandas as pd
+def kpi_total_sales(df, sales_col):
+    return df[sales_col].sum()
 
-def calculate_kpis(df):
-    """Calculate basic KPIs dynamically"""
-    total_orders = len(df)
-    total_quantity = df.select_dtypes(include=['int', 'float']).sum().sum() if not df.empty else 0
-    total_amount = df['AMOUNT'].sum() if 'AMOUNT' in df.columns else 0
-    total_rejected = df['REJECTED_ORDER_COMMENT'].notna().sum() if 'REJECTED_ORDER_COMMENT' in df.columns else 0
-    
-    kpis = {
-        "Total Orders": total_orders,
-        "Total Quantity": total_quantity,
-        "Total Amount": total_amount,
-        "Rejected Orders": total_rejected
-    }
-    return kpis
+
+def kpi_aov(df, sales_col):
+    return df[sales_col].mean()
+
+
+def kpi_orders(df):
+    return len(df)
