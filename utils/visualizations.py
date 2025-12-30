78a62823edddbbfe713bcaf10bf6c6269451b2a5
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-30 23:10:08 +0530

    Update visualizations.py

--- utils/visualizations.py
+++ utils/visualizations.py
@@ -1,15 +1,15 @@
 # utils/visualizations.py
+
+import pandas as pd
 import plotly.express as px
 
 def plot_orders_over_time(df, date_col):
     df[date_col] = pd.to_datetime(df[date_col], errors='coerce')
-    orders_time = df.groupby(date_col).size().reset_index(name='Order Count')
-    fig = px.line(orders_time, x=date_col, y='Order Count', title='Orders Over Time')
+    df_grouped = df.groupby(date_col).sum().reset_index()
+    fig = px.line(df_grouped, x=date_col, y='AMOUNT', title='Orders Over Time')
     return fig
 
 def plot_top_categories(df, col, value_col='AMOUNT', top_n=10):
-    if col not in df.columns:
-        return None
-    top_items = df.groupby(col)[value_col].sum().sort_values(ascending=False).head(top_n).reset_index()
-    fig = px.bar(top_items, x=col, y=value_col, title=f'Top {top_n} {col} by {value_col}', text=value_col)
+    top = df.groupby(col)[value_col].sum().sort_values(ascending=False).head(top_n).reset_index()
+    fig = px.bar(top, x=col, y=value_col, title=f'Top {top_n} {col}')
     return fig
