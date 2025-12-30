981230861609dd00e9d0dde1e47eeb64e48dc816
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-31 00:51:15 +0530

    Update forecasting.py

--- utils/forecasting.py
+++ utils/forecasting.py
@@ -1,30 +1,52 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import numpy as np
+# utils/forecasting.py
 
-def sales_forecast(df, date_col, sales_col, periods=6):
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+def prepare_time_series(df, date_col, sales_col, freq="M"):
+    """
+    Prepare aggregated time series data
+    """
     temp = df[[date_col, sales_col]].copy()
-    temp[date_col] = pd.to_datetime(temp[date_col])
-    temp = temp.groupby(date_col)[sales_col].sum().reset_index()
+    temp[date_col] = pd.to_datetime(temp[date_col], errors="coerce")
+
+    temp = (
+        temp
+        .groupby(pd.Grouper(key=date_col, freq=freq))[sales_col]
+        .sum()
+        .reset_index()
+        .dropna()
+    )
 
     temp["t"] = np.arange(len(temp))
+    return temp
 
-    X = temp[["t"]]
-    y = temp[sales_col]
+
+def forecast_sales(ts_df, periods=6):
+    """
+    Forecast future sales using Linear Regression
+    """
+    X = ts_df[["t"]]
+    y = ts_df.iloc[:, 1]  # sales column
 
     model = LinearRegression()
     model.fit(X, y)
 
-    future_t = np.arange(len(temp), len(temp) + periods)
+    future_t = np.arange(len(ts_df), len(ts_df) + periods)
+
     future_sales = model.predict(future_t.reshape(-1, 1))
 
-    future_df = pd.DataFrame({
-        date_col: pd.date_range(
-            start=temp[date_col].max(),
-            periods=periods + 1,
-            freq="M"
-        )[1:],
-        sales_col: future_sales
+    future_dates = pd.date_range(
+        start=ts_df.iloc[-1, 0],
+        periods=periods + 1,
+        freq="M"
+    )[1:]
+
+    forecast_df = pd.DataFrame({
+        ts_df.columns[0]: future_dates,
+        ts_df.columns[1]: future_sales
     })
 
-    return temp, future_df
+    return forecast_df
