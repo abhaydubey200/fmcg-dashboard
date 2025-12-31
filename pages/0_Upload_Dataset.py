ac0d26ef803a66b344f4824af8592f294079c559
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-31 10:26:29 +0530

    Update 0_Upload_Dataset.py

--- pages/0_Upload_Dataset.py
+++ pages/0_Upload_Dataset.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from utils.data_loader import load_dataset
 
-st.header("📤 Upload FMCG Dataset")
+st.header(" Upload FMCG Dataset")
 
 uploaded_file = st.file_uploader("Upload CSV or Excel", type=["csv", "xlsx"])
 
 if uploaded_file:
     df = load_dataset(uploaded_file)
     if df is not None:
-        st.success("✅ Dataset loaded successfully")
+        st.success(" Dataset loaded successfully")
         st.dataframe(df.head())
