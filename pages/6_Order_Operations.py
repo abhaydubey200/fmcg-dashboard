3d88673d13fc5a035c36c33dc041d801bf6c8b62
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-31 10:29:35 +0530

    Update 6_Order_Operations.py

--- pages/6_Order_Operations.py
+++ pages/6_Order_Operations.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.column_detector import auto_detect_columns
 from utils.visualizations import bar_top
 
-st.header("🚚 Order & Operations Dashboard")
+st.header(" Order & Operations Dashboard")
 
 df = st.session_state.get("df")
 if df is None:
