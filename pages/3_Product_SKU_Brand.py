26ab61b8d83fe51722733db2e094eddc6abbb4d5
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2025-12-31 10:28:34 +0530

    Update 3_Product_SKU_Brand.py

--- pages/3_Product_SKU_Brand.py
+++ pages/3_Product_SKU_Brand.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.column_detector import auto_detect_columns
 from utils.visualizations import bar_top
 
-st.header("📦 Product / SKU / Brand Dashboard")
+st.header(" Product / SKU / Brand Dashboard")
 
 df = st.session_state.get("df")
 if df is None:
